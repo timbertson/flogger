13de347a630c1ea005d73d425cf72a0e754b526a
Tim Cuthbertson <tim@gfxmonk.net>
2024-07-26 13:26:40 +1000

    bump scala docker image

--- choredefs/index.ts
+++ choredefs/index.ts
@@ -1,3 +1,3 @@
-import { default as Scala } from 'https://raw.githubusercontent.com/timbertson/chored-timbertson/ec7b194a600557dbdfe7444f706f567e096435c3/lib/scala.ts#main'
+import { default as Scala } from 'https://raw.githubusercontent.com/timbertson/chored-timbertson/5cb458071d50ad8b30b26a259888f2f80c39eae7/lib/scala.ts#main'
 
 export default Scala({ repo: 'flogger', scalaMajorVersions: [3] })
